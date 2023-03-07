6cbee9e6e24f57d5c8040066f66a5772a9cdb143
GirbuNicolae <girbu.nicolae.mail@gmail.com>
2023-03-07 20:41:18 +0200

    Add pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,15 +1,16 @@
 import BootrapPagination from "react-bootstrap/Pagination";
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import styles from "./Pagination.module.css";
 
 export function Pagination(props) {
   let { active, baseUrl } = props;
-  const history = useHistory();
+  const navigate = useNavigate();
   if (!active) {
     active = 1;
   }
 
   const onPageClick = page => {
-    history.push(`${baseUrl}?page=${page}`);
+    navigate(`${baseUrl}?page=${page}`);
     window.scrollTo({
       top: 0,
       behavior: "smooth",
@@ -17,12 +18,12 @@ export function Pagination(props) {
   };
 
   return (
-    <div>
-      <BootrapPagination>
+    <div className='d-flex justify-content-center mb-5'>
+      <BootrapPagination className={styles.pagination}>
         {[1, 2, 3, 4, 5].map(page => (
           <BootrapPagination.Item
             key={page}
-            active={page === active}
+            active={page === Number(active)}
             onClick={() => onPageClick(page)}>
             {page}
           </BootrapPagination.Item>
