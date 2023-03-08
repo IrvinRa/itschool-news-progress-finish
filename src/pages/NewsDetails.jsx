f156c590acc893624d80542c52efb3f36a3a06b2
GirbuNicolae <girbu.nicolae.mail@gmail.com>
2023-03-08 10:36:49 +0200

    Update repo with solution

--- src/pages/NewsDetails.jsx
+++ src/pages/NewsDetails.jsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { useContext } from "react";
+import { Alert } from "react-bootstrap";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { getNewsDetails } from "../api/adapters";
@@ -8,19 +11,36 @@ import { addToFavorites } from "../store/Favorites/actions";
 import { FavoritesContext } from "../store/Favorites/context";
 import { getFormattedDate } from "../utils/date";
 import { useFetch } from "../utils/hooks/useFetch";
+import { useLocalStorage } from "../utils/hooks/useLocalStorage";
 import styles from "./NewsDetails.module.css";
 
 export function NewsDetails() {
   const params = useParams();
   const newsId = params.newsId + "/" + params["*"];
 
-  const { favoritesDispatch } = useContext(FavoritesContext);
+  const { favoritesState, favoritesDispatch } = useContext(FavoritesContext);
 
   const newsDetailsEndpoint = getNewsDetailsEndpoint(newsId);
 
   const newsDetails = useFetch(newsDetailsEndpoint);
 
   const adaptedNewsDetails = getNewsDetails(newsDetails);
+
+  // Adaugam un state pentru afisarea alertei.
+  const [isAlertDisplayed, setIsAlertDisplayed] = useState(false);
+  // Extragem functia de modificare a localStorage-ului. Nu avem nevoie de state-ul din localStorage, conventia este ca pentru variabile neutilizate sa punem denumirea _.
+  // Comentariul eslint-disable-next-line dezactiveaza eslint pentru urmatoarea linie (sa nu se planga ca nu utilizam variabila _).
+  // eslint-disable-next-line
+  const [_, setLocalStorageState] = useLocalStorage(
+    "favorites",
+    favoritesState
+  );
+  // Adaugarea in localStorage este un efect, atunci cand se modifica produsele favorite.
+  // Cum strim ca s-au modificat produsele favorite? Primim o noua valoare a lui favoritesState.
+  // setLocalStorageState este sugerat sa fie adaugat la dependente de o regula de lining.
+  useEffect(() => {
+    setLocalStorageState(favoritesState);
+  }, [favoritesState, setLocalStorageState]);
 
   const { title, description, image, date, author, content, thumbnail } =
     adaptedNewsDetails;
@@ -36,10 +56,23 @@ export function NewsDetails() {
 
     const actionResult = addToFavorites(newsItem);
     favoritesDispatch(actionResult);
+
+    // Afisam alerta.
+    setIsAlertDisplayed(true);
+    // Imediat ce afisam alerta, declansam un timer, care in 2 secunde va ascunde alerta.
+    setTimeout(() => {
+      setIsAlertDisplayed(false);
+    }, 2000);
   };
 
   return (
     <Layout>
+      {/* Afisam conditionat alerta */}
+      {isAlertDisplayed && (
+        <Alert variant='success' id={styles.alert}>
+          Succes! Poți vedea știrea accesând secțiunea Favorite.
+        </Alert>
+      )}
       <Container className={`${styles.newsDetails} text-start my-5`}>
         <Row className='justify-content-center'>
           <Col xs={12} lg={8}>
--- src/store/Favorites/context.js
+++ src/store/Favorites/context.js
@@ -1,12 +1,19 @@
 import { useReducer } from "react";
 import { createContext } from "react";
+import { useLocalStorage } from "../../utils/hooks/useLocalStorage";
 import { favoritesReducer, initialState } from "./reducer";
 
 export const FavoritesContext = createContext();
 
 export const FavoritesProvider = props => {
   const { children } = props;
-  const [state, dispatch] = useReducer(favoritesReducer, initialState);
+  // Initializam state-ul, pornind de la localStorage.
+  // Daca am state in localStorage, il preiau, daca nu pornesc de la initialState.
+  const [initialLocalStorageState] = useLocalStorage("favorites", initialState);
+  const [state, dispatch] = useReducer(
+    favoritesReducer,
+    initialLocalStorageState
+  );
 
   const favoritesContextValue = {
     favoritesState: state,
